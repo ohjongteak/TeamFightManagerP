abec74f10d5490d15d11d08aa751fd4595d7e976
오종택 <ejjt258@rp5entartainment.com>
2023-05-01 16:27:07 +0900

    챔피언 정보 정렬
    챔피언 정보 창 포지션 버튼 클릭 시 포지션 정렬

--- Assets/ChampListSort.cs
+++ Assets/ChampListSort.cs
@@ -24,7 +24,7 @@ public class ChampListSort : MonoBehaviour
             if (CharacterStateArray[i].Unlock == "true")
             {
                 GameObject ChampBox = Instantiate(champBoxPrefab, champListPanel);
-
+                champIntroBox.Add(ChampBox);
                 ChampBox.transform.GetChild(0).GetComponent<Image>().sprite = imgManager.champSprite[i];
                 ChampBox.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = CharacterStateArray[i].positionName;
                 ChampBox.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = CharacterStateArray[i].characterName;
@@ -33,7 +33,67 @@ public class ChampListSort : MonoBehaviour
 
         }
 
+       
 
     }
+
+    public void AllChamp()
+    {
+        for(int i = 0; i<champIntroBox.Count; i++)
+        {
+            champIntroBox[i].SetActive(true);
+        }
+
+    }
+    public void Assasin()
+    {
+        var CharacterStateArray = characterJsonRead.characterStateList.characterState;
+
+        
+    }
+
+    public void Warrior()
+    {
+        var CharacterStateArray = characterJsonRead.characterStateList.characterState;
+
+        for(int i =0; i<champIntroBox.Count; i++)
+        {
+            if(CharacterStateArray[i].indexCharacter / 100 == 1)
+            {
+                champIntroBox[i].gameObject.SetActive(true);
+            }
+            else
+                champIntroBox[i].gameObject.SetActive(false);
+        }
+
+    }
+
+    public void MarkMan()
+    {
+        var CharacterStateArray = characterJsonRead.characterStateList.characterState;
+
+
+        for (int i = 0; i < champIntroBox.Count; i++)
+        {
+            if (CharacterStateArray[i].indexCharacter / 100 == 2)
+            {
+                champIntroBox[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                champIntroBox[i].gameObject.SetActive(false);
+                
+            }
+        }
+    }
+
+    public void Magicion()
+    {
+        var CharacterStateArray = characterJsonRead.characterStateList.characterState;
+
+
+    }
+
+   
 
 }
