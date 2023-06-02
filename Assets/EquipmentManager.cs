eddc5cdddd9258f3fdeb60aed281735465633c0c
오종택 <ejjt258@rp5entartainment.com>
2023-06-02 15:59:18 +0900

    장비 Json 구조 수정

--- Assets/EquipmentManager.cs
+++ Assets/EquipmentManager.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 namespace Framework.UI
 {
     public enum EquipStateType
     {
         None,
-        Attack,
-        Defend,
-        Cooltime,
-        TypeMastery,
-        AttackSpeed
+        공격력증가,
+        방어력증가,
+        쿨타임감소,
+        계열숙련도,
+        공격속도
 
     }
 
@@ -35,19 +36,18 @@ namespace Framework.UI
         public int[] arrChairStatePoint;
         public int[] arrUniformStatePoint;
 
+        public string[] arrHeadsetName;
+        public string[] arrControllerName;
+        public string[] arrChairName;
+        public string[] arrUniformName;
+
     }
 
    
     public class EquipmentManager : MonoBehaviour
     {
         [SerializeField]
-        private Image wearHeadset;
-        [SerializeField]
-        private Image wearController;
-        [SerializeField]
-        private Image wearChair;
-        [SerializeField]
-        private Image wearUniform;
+        private Transform[] arrEquipChild;
         [SerializeField]
         private ImageManager imgManager;
 
@@ -55,38 +55,36 @@ namespace Framework.UI
         public TextAsset equipInfoText;
         public PlayerInformation playerInformation;
 
+
         public void Init()
         {
             equipInfoText = Resources.Load("equipInfo") as TextAsset;
             equipInfoCollet = JsonUtility.FromJson<EquipInfoCollect>(equipInfoText.text);
 
 
-
+            //Debug.Log(playerInformation.GetPlayerInfo().arrWearHeadset);
             for (int i = 0; i < playerInformation.GetPlayerInfo().arrWearHeadset.Length; i++)
             {
                 if (playerInformation.GetPlayerInfo().arrWearHeadset[i] == true)
                 {
-                    wearHeadset.sprite = imgManager.arrHeadset[i];
+                    arrEquipChild[0].GetChild(0).GetComponent<Image>().sprite = imgManager.arrHeadset[i];
+                    arrEquipChild[0].GetChild(1).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrHeadsetName[i];
+                    arrEquipChild[0].GetChild(2).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrHeadsetStateType[i + 1].ToString();
+                    arrEquipChild[0].GetChild(3).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrHeadsetStatePoint[i].ToString();
                     break;
                 }
 
             }
 
-            for (int i = 0; i < playerInformation.GetPlayerInfo().arrWearUniform.Length; i++)
-            {
-                if (playerInformation.GetPlayerInfo().arrWearUniform[i] == true)
-                {
-                    wearUniform.sprite = imgManager.arrUniform[i];
-                    break;
-                }
-
-            }
 
             for (int i = 0; i < playerInformation.GetPlayerInfo().arrWearController.Length; i++)
             {
                 if (playerInformation.GetPlayerInfo().arrWearController[i] == true)
                 {
-                    wearController.sprite = imgManager.arrController[i];
+                    arrEquipChild[1].GetChild(0).GetComponent<Image>().sprite = imgManager.arrController[i];
+                    arrEquipChild[1].GetChild(1).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrControllerName[i];
+                    arrEquipChild[1].GetChild(2).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrControllerStateType[i + 1].ToString();
+                    arrEquipChild[1].GetChild(3).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrControllerStatePoint[i].ToString();
                     break;
                 }
 
@@ -96,7 +94,23 @@ namespace Framework.UI
             {
                 if (playerInformation.GetPlayerInfo().arrWearChair[i] == true)
                 {
-                   wearChair.sprite = imgManager.arrChair[i];
+                    arrEquipChild[2].GetChild(0).GetComponent<Image>().sprite = imgManager.arrChair[i];
+                    arrEquipChild[2].GetChild(1).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrChairName[i];
+                    arrEquipChild[2].GetChild(2).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrChairStateType[i + 1].ToString();
+                    arrEquipChild[2].GetChild(3).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrChairStatePoint[i].ToString();
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i < playerInformation.GetPlayerInfo().arrWearUniform.Length; i++)
+            {
+                if (playerInformation.GetPlayerInfo().arrWearUniform[i] == true)
+                {
+                    arrEquipChild[3].GetChild(0).GetComponent<Image>().sprite = imgManager.arrUniform[i];
+                    arrEquipChild[3].GetChild(1).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrUniformName[i];
+                    arrEquipChild[3].GetChild(2).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrUniformStateType[i + 1].ToString();
+                    arrEquipChild[3].GetChild(3).GetComponent<TextMeshProUGUI>().text = equipInfoCollet.equipInfo.arrUniformStatePoint[i].ToString();
                     break;
                 }
 
@@ -104,11 +118,7 @@ namespace Framework.UI
 
         }
 
-        private void Start()
-        {
-            Init();
-
-        }
+        
     }
 
 }
